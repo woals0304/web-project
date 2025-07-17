490218a06dbf17ac334ea1957ab65a1a78e34f6f
Namgung Jaemin <woals0304@naver.com>
2025-07-17 14:38:42 +0900

    feat: update web-streams-polyfill to version 3.3.3 and add new package configurations
    - Added TSDoc metadata for documentation tools.
    - Created package.json files for es6 and es2018 builds of web-streams-polyfill.
    - Updated main package.json for web-streams-polyfill with new scripts and dependencies.
    - Introduced ponyfill versions for es6 and es2018.
    - Updated project version to 1.0.0 and changed license to Proprietary.
    - Replaced debug with cors and added dotenv and sweetalert2 as dependencies.

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 import express from 'express';
-import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
+
 const port = 8080;
 
-// 정적 파일 서빙을 위한 디렉토리 설정
-app.use(express.static('public'));
+// 현재 파일의 URL에서 디렉토리 경로를 추출
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// 루트 경로에 대한 요청 처리
-app.get('/', (request, response) => {
-    return response.sendFile(join(process.cwd(), 'public/index.html'));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.redirect('/html/index.html');
 });
 
-// 서버 시작
 app.listen(port, () => {
-    console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+    console.log(`Server is running on port ${port}`);
 });
